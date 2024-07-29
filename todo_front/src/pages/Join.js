a14e567354d07bc502690d4e7d887219b1765fc4
jhrogh <wogml5031@gmail.com>
2024-07-29 17:08:29 +0900

    240729/front/Join.js, Login.js 마크업 수정(1)

--- todo_front/src/pages/Join.js
+++ todo_front/src/pages/Join.js
@@ -1,6 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import '../assets/styles/JoinStyle.css';
 
-function Join() {}
+function Join() {
+  return (
+    <div className="join-container">
+      <div className='join-body'>
+        <div className="join-title">회원가입</div>
+        <div className="join-input">
+          <label>*아이디</label>
+          <input
+            className="join-input-id"
+            type="text"
+            placeholder="아이디를 입력해주세요."
+          />
+        </div>
+        <div className="join-input">
+          <label>*이름</label>
+          <input className="join-input-name" type="text" />
+        </div>
+        <div className="join-input">
+          <label>*비밀번호</label>
+          <input
+            className="join-input-pw"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+          />
+        </div>
+        <div className="join-input">
+          <label>*비밀번호 확인</label>
+          <input
+            className="join-input-pwcheck"
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+          />
+        </div>
+        <div className="join-input">
+          <label>*이메일</label>
+          <input className="join-input-email" type="email" />
+        </div>
+        <button className="join-button">가입하기</button>
+      </div>
+    </div>
+  );
+}
 
 export default Join;
