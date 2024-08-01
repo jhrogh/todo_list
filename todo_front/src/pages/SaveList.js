7565477bf4ca31f93c452cc1e82d9277d57489b8
jhrogh <wogml5031@gmail.com>
2024-08-01 16:09:29 +0900

    240801/back/환경설정- DB 연동, CORS 설정

--- todo_front/src/pages/SaveList.js
+++ todo_front/src/pages/SaveList.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import '../assets/styles/SaveListStyle.css';
 
-function SaveList() {}
+function SaveList() {
+  const posts = [
+    { id: 1, title: '2024.07.31.16:30' },
+    { id: 2, title: '여행 준비물' },
+    { id: 3, title: '2024.06.15.15:33' },
+    { id: 4, title: '글제목 수정 중' }
+  ];
+
+  return (
+    <div className="todo-list-container">
+      {posts.map(post => (
+        <div key={post.id} className="todo-item">
+          <span>{post.title}</span>
+          <div className="icon-group">
+            <img
+              src="/images/edit.png"
+              alt="수정"
+              className="edit-icon"
+            />
+            <img
+              src="/images/delete.png"
+              alt="삭제"
+              className="delete-icon"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default SaveList;
