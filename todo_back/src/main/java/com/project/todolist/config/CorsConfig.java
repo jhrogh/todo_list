f98f0093c060667bbe5a3043563c63242094b106
jhrogh <wogml5031@gmail.com>
2024-08-22 20:37:14 +0900

    240822/front/불필요한 파일 제거

--- todo_back/src/main/java/com/project/todolist/config/CorsConfig.java
+++ todo_back/src/main/java/com/project/todolist/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://43.202.173.195:3000", "http://43.202.173.195")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
